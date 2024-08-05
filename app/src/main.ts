63db3a48203061aa47008c89dcf4a28b1335de79
5h1ngy <sig.scarano@outlook.it>
2024-08-05 15:09:22 +0200

    chore: gsap animations

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,6 +1,13 @@
 import * as PIXI from 'pixi.js';
+import { gsap } from "gsap";
+import { PixiPlugin } from "gsap/PixiPlugin";
+
 import '../style.css'
 
+// register the plugin
+gsap.registerPlugin(PixiPlugin);
+// give the plugin a reference to the PIXI object
+PixiPlugin.registerPIXI(PIXI);
 
 // Create the application helper and add its render target to the page
 const app = new PIXI.Application();
@@ -12,16 +19,17 @@ let sprite = PIXI.Sprite.from(await PIXI.Assets.load('public/simple_sprite.png')
 app.stage.addChild(sprite);
 
 // keyboard event binding
-let moveSprite = false;
 document.addEventListener('keydown', () => {
-  moveSprite = true;
+  //new way (with plugin):
+  gsap.to(sprite, {
+    pixi: { scaleX: 2, scaleY: 1.5, skewX: 30, rotation: 60 },
+    duration: 1,
+  });
 })
 
 // Add a ticker callback to move the sprite back and forth
 let elapsed = 0.0;
 app.ticker.add((ticker) => {
   elapsed += ticker.deltaTime;
-  if (moveSprite) {
-    sprite.x = 100.0 + Math.cos(elapsed / 50.0) * 100.0;
-  }
+  sprite.x = 100.0 + Math.cos(elapsed / 50.0) * 100.0;
 });
